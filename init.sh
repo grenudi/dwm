90aa341634d3cd44c5eba24523596cbf7265e53e
grenudi <idunerg@gmail.com>
2020-08-19 21:30:25 +0300

    now in pink with xinitrc!

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 #determine distro
+
 DISTRO=$(sudo cat /etc/os-release | grep '^NAME' | grep -o '\".*\"' | grep -o "\w*");
+
+#run install dependency for arch or void
 if [ "$DISTRO" = 'void' ]
 then
 	echo "$DISTRO >> THIS IS VOID LINUX"
@@ -10,4 +13,16 @@ else
 	echo "$DISTRO >> THIS IS NOT VOID TRYING ARCH"
 	sh 'init/instarch.sh'
 fi
-#run install dependency for arch or void
+
+#install DWM
+sudo make clean install;
+
+#determine if i've already paste in initrc
+INITRCMARK=$(cat ~/.initrc | grep -o "#DWMMARK");
+if [ "$INITRCMARK" = "#DWMMARK" ]
+then
+	echo "\nINITRC NOTHING TO DO, ALREDY PATCHED\n"
+else
+	echo "\nPATCHING INITRC\n"
+	cp init/xinitrc ~/.xinitrc
+fi
--- init/instvoid.sh
+++ init/instvoid.sh
@@ -3,5 +3,6 @@ sudo xbps-install -Sy base-devel ;
 sudo xbps-install -Sy libX11-devel;
 sudo xbps-install -Sy libXft-devel;
 sudo xbps-install -Sy libXinerama-devel; 
-sudo xbps-install -Sy dmenu ;
+sudo xbps-install -Sy dmenu;
+sudo xbps-install -Sy xorg;
 echo "DEPENDENCIES INSTALLED FOR VOID"
